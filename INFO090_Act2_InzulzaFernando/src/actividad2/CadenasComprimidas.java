41d40daabb05af83095784d283c2fedd8d138c4d
Ferinz-o <olavarriafernando36@gmail.com>
2022-04-03 22:43:12 -0400

    comentarios en CadenasComprimidas

--- INFO090_Act2_InzulzaFernando/src/actividad2/CadenasComprimidas.java
+++ INFO090_Act2_InzulzaFernando/src/actividad2/CadenasComprimidas.java
@@ -7,11 +7,23 @@ public class CadenasComprimidas {
 	public static void main(String[] args) {
 		ArrayList<String> entrada = //ALMACENA LINEAS DEL ARCHIVO DE TEXTO EN UN ARRAY
 				AccesoArchivosArrayList.leerArchivo("./data/cadenasComprimidas/entrada.txt");
-		
+		String a = "1234567890";
+		String b = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		for(String s : entrada) {
+			String str = ""; 
 			if(s.length()>100 || s.length()<1) {
-				
+				System.out.println("ERROR:El archivo no cumple con lo indicado");
+				System.exit(0);
 			}
+			
+//			for(int i=0;i<s.length();i++) {
+//				10A COMPROBARA EL NUMERO EN "a" Y CUANDO ENCUENTRE ALGO DE "b" ENTONCES:
+//				char letra = 'A'; SACA EL CARACTER
+//				int num = 10; SACA LA CANTIDAD DE VECES QUE SE AGREGARA EL CARACTER
+//				for(int j = 0; j<num;j++) { HARA EL PROCESO DE AGREGAR LO ANTERIOR MENCIONADO AL STRING "str"
+//					str = str + letra;
+//				}
+//			}
 		}
 
 	}
