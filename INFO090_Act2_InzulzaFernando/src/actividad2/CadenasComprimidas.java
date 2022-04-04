69ea45eba9c0c001a123667636c7db6a57b2a2e0
Lukayx <olavarriafernando36@gmail.com>
2022-04-04 17:16:04 -0400

    Solo falta verificar letra despues de nuemero

--- INFO090_Act2_InzulzaFernando/src/actividad2/CadenasComprimidas.java
+++ INFO090_Act2_InzulzaFernando/src/actividad2/CadenasComprimidas.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 
 public class CadenasComprimidas {
 
-	public static void compruebaError(String A,String B,String letra) {
+	public static boolean compruebaError(String A,String B,String letra) {
 		if(A.contains(letra) != true && B.contains(letra) != true) { //SI NO ESTA EN NINGUNA DE LOS 2 ENTONCES FINALIZA EL CODIGO
-			System.out.println("ERROR:El archivo no cumple con lo indicadou");
-			System.exit(0);
+			return true;
 		}
+		return false;
 	}
 	
 	public static byte estado(String A,String B,String letra){
@@ -27,42 +27,41 @@ public class CadenasComprimidas {
 		byte e;
 		for(String s : entrada) {
 			if(s.length()>80){ // COMPRUEBA SI LA CADENA NO SUPERA EL MAXIMO DE CARACTERES
-				System.out.println("ERROR:El archivo no cumple con lo indicado");
-				System.exit(0);
-			}
-			
-			letra = String.valueOf(s.charAt(0)); // RECOGE EL CARACTER EN LA POSICION INDICADA
-			compruebaError(A,B,letra);
-			e = estado(A,B,letra);
-			if(A.contains(letra)) {
-				num += letra;
+				str="ERROR";
 			} else {
-				str += letra;
-			}
-			for(int i=1;i<s.length();i++) {
-				letra = String.valueOf(s.charAt(i));
-				compruebaError(A,B,letra);
-				
-				if(e==0 && A.contains(letra) || e==1 && A.contains(letra)) {
+				letra = String.valueOf(s.charAt(0)); // RECOGE EL CARACTER EN LA POSICION INDICADA
+				if(compruebaError(A,B,letra)) {break;}
+				e = estado(A,B,letra);
+				if(A.contains(letra)) {
 					num += letra;
-					e = 0;
-				} else if(e==0 && B.contains(letra)) {
-					for(int j=0;j<Integer.parseInt(num);j++) {
-						str += letra;
-					}
-					num = "";
 				} else {
 					str += letra;
 				}
-				e = estado(A,B,letra);
+				for(int i=1;i<s.length();i++) {
+					letra = String.valueOf(s.charAt(i));
+					
+					if(compruebaError(A,B,letra)){break;}
+					
+					if(e==0 && A.contains(letra) || e==1 && A.contains(letra)) {
+						num += letra;
+						e = 0;
+					} else if(e==0 && B.contains(letra)) {
+						for(int j=0;j<Integer.parseInt(num);j++) {
+							str += letra;
+						}
+						num = "";
+					} else {
+						str += letra;
+					}
+					e = estado(A,B,letra);
+				}
+				if(compruebaError(A,B,letra)) {str="ERROR";}
+				linea.add(str);
+				str = "";
 			}
-			
-			if(str.length()>80) {
-				str="ERROR";
-			}
-			linea.add(str);
-			str = "";
 		}
+			
+			
 		AccesoArchivosArrayList.escribirArchivo("./data/cadenasComprimidas/salida.txt",linea); //CREARA ARCHIVO DE SALIDA
 
 	}
