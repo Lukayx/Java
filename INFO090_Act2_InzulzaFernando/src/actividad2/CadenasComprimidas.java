6f4d597a9ae26b4e83866237876d526cc7a23527
Ferinz-o <olavarriafernando36@gmail.com>
2022-04-04 23:55:59 -0400

    TODO BUENO DEFINITIVO

--- INFO090_Act2_InzulzaFernando/src/actividad2/CadenasComprimidas.java
+++ INFO090_Act2_InzulzaFernando/src/actividad2/CadenasComprimidas.java
@@ -11,20 +11,21 @@ public class CadenasComprimidas {
 		return false;
 	}
 	
-	public static byte estado(String A,String B,String letra){ // |1 SI ESTA EN B | 0 SI ESTA EN A |
+	public static boolean estado(String A,String B,String letra){ // |TRUE SI ES LETRA | FALSE SI ES NUMERO|
 		if(A.contains(letra)){ 
-			return 0;
+			return false;
 		} else { 
-			return 1;
+			return true;
 		}
 	}
+	
 	public static void main(String[] args) {
 		ArrayList<String> entrada = // ALMACENA LINEAS DEL ARCHIVO DE TEXTO EN UN ARRAY
 				AccesoArchivosArrayList.leerArchivo("./data/cadenasComprimidas/entrada.txt");
 		final String A = "1234567890", B = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		String letra,num = "",str = "";
-		ArrayList<String> linea = new ArrayList<>();
-		byte e;
+		ArrayList<String> linea = new ArrayList<>(); //FUTURO ARRAY CON LINEAS DE ARCHIVO SALIDA
+		String letra,num = "",str = ""; // POSICION CARACTER / FACTOR DE MULTIPLICACION / CADENA DESCOMPRIMIDA
+		boolean e;	
 		for(String s : entrada) {
 			letra = String.valueOf(s.charAt(0));// SACA EL CARACTER EN LA POSICION 0 DE LA LINEA
 			//SIGNIFICADO IF:| SI CASO NO ES MENOR O IGUAL A 80 |or| CARACTER NI NUMERO NI LETRA |
@@ -44,12 +45,12 @@ public class CadenasComprimidas {
 						str="ERROR";
 						break;
 					}
-					if(e==0 && A.contains(letra) || e==1 && A.contains(letra)) {
+					if(e == false && A.contains(letra) || e == true && A.contains(letra)) {
 						num += letra;
-						e = 0;
-					} else if(e==0 && B.contains(letra)) {
+						e = false;
+					} else if(e == false && B.contains(letra)) {
 						// ESTO VERIFICA SI 1<num<100
-						if(Integer.parseInt(num)>=100 || Integer.parseInt(num)==0) {str="ERROR"; break;} 
+						if(Integer.parseInt(num)>=100 || Integer.parseInt(num)<=1) {str="ERROR"; break;} 
 						//MULTIPLICA LETRAS Y LAS AGREGA A UN STRING
 						for(int j=0;j<Integer.parseInt(num);j++) {
 							str += letra;
@@ -58,10 +59,9 @@ public class CadenasComprimidas {
 					} else {
 						str += letra;
 					}
-					e = estado(A,B,letra);
-					// PARA ARREGLARLO HAZ QUE SI "E" ES 0 Y CUANDO SALE NO ES 1 PUES ESTA MALO Y SALTA ERROR 
+					e = estado(A,B,letra); //EL ESTADO SE ACTUALIZA AL FINAL
 				}
-				if(e==0) { str="ERROR";	} //VERIFICA SI EL ULTIMO CARACTER DE LA LINEA ES UN NUMERO
+				if(e==false) { str="ERROR";	} //HAY FACTOR DE MULTIPLICACION PERO NO LETRA A MULTIPLICAR
 			}
 				linea.add(str);
 				str = "";
