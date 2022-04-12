fee4ac57fc3ad32bf5933042430f273a1e7f9186
Lukayx <olavarriafernando36@gmail.com>
2022-04-12 11:15:19 -0400

    muchas cosas completadas

--- Clases en Uach/src/cl/uach/info090/semana03/Clase1.java
+++ Clases en Uach/src/cl/uach/info090/semana03/Clase1.java
@@ -3,11 +3,12 @@ package cl.uach.info090.semana03;
 public class Clase1 {
 	
 	public static void main(String[] args) {
-		//Car myCar = new Car();
-		// myCar.brand = "Ultima";
-		
-		
-		//System.out.println(myCar.getBrand());
+		Car myCar = new Car();
+		System.out.println(myCar.getLicensePlate());
+		myCar.setLicensePlate("Ultima");
+		System.out.println(myCar.getLicensePlate());
+		myCar = new Car();
+		System.out.println(myCar.getLicensePlate());
 	}
 
 }
---
+++ INFO090_Act3_InzulzaFernando/src/Actividad3/Campo.java
@@ -0,0 +1,61 @@
+package Actividad3;
+
+import java.util.ArrayList;
+
+public class Campo {
+	private char original[][];
+	private char resultado[][];
+	private int N_fil,N_col;
+	
+	public Campo(ArrayList<String> lineas) {
+		int i = 0;
+		for(String s : lineas) {
+			for (int j = 0; j < s.length(); j++) {
+				original[i][j] = s.charAt(j);				
+			}
+			i++;
+		}
+		N_fil = original.length;
+		N_col = original[0].length;
+	}
+	
+	public boolean compruebaIndices(int i,int j){
+		return i<N_fil && i>=0 && j>=0 && j<N_col && original[i][j]=='*';
+	}
+	
+	public int compruebaMinas(int i,int j) {
+		int c = 0;
+		if(compruebaIndices(i-1,j-1)){c++;}
+		if(compruebaIndices(i-1,j)){c++;}
+		if(compruebaIndices(i-1,j+1)){c++;}
+		if(compruebaIndices(i,j-1)){c++;}
+		if(compruebaIndices(i,j+1)){c++;}
+		if(compruebaIndices(i+1,j-1)){c++;}
+		if(compruebaIndices(i+1,j)){c++;}
+		if(compruebaIndices(i+1,j+1)){c++;}
+		return c;
+	}
+	
+	public String campoListo(){
+		for (int i = 0; i < N_fil; i++) {
+			for (int j = 0; j < N_col; j++) {
+				if(original[i][j]=='*') {
+					resultado[i][j]='*';
+				} else {
+					resultado[i][j] = (char)compruebaMinas(i,j);
+				}
+			}
+		}
+		
+		String str="";
+		for (int i = 0; i < N_fil; i++) {
+			for (int j = 0; j < N_col; j++) {
+				str+=resultado[i][j];
+			}
+			str+="\n";
+		}
+		return str;
+	}
+}
+	
+
