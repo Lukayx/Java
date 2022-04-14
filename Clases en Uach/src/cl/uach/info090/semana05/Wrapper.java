eac7fbc291e1f61aa85113b08c0cf984080cbaca
Lukayx <olavarriafernando36@gmail.com>
2022-04-14 14:18:15 -0400

    wrapper

--- Clases en Uach/src/cl/uach/info090/semana05/Wrapper.java
+++ Clases en Uach/src/cl/uach/info090/semana05/Wrapper.java
@@ -11,12 +11,18 @@ public class Wrapper {
 		int c = a + Integer.parseInt("123");
 		System.out.println(c+b);
 		ArrayList<Integer> vector = new ArrayList<>();
-		vector.add(new Integer(150));
+		vector.add(new Integer(150)); //YA ESTÁ OBSOLETO
 		vector.add(30);
 		vector.add(-170);
 		vector.add(55);
 		Collections.sort(vector);
-		System.out.println(vector);
+		
+		//int[][] gato = new int[4][4];
+		ArrayList<ArrayList<Double>> l = new ArrayList<>(); 
+		//Double g = l.get(0).get(0);
+		//System.out.println(g);
+		ArrayList<Double[]> n; //n[0].get(0);
+		ArrayList<Double>[] m; //m.get(0)[0];
 	}
 
 }
