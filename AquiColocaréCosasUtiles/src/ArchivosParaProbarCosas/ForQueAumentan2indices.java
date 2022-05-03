3c7fbd4ec8e70382d110a2a6fa02772ce4cd84ee
Ferinz-o <olavarriafernando36@gmail.com>
2022-05-03 00:04:06 -0400

    polinomio

---
+++ AquiColocaréCosasUtiles/src/ArchivosParaProbarCosas/ForQueAumentan2indices.java
@@ -0,0 +1,13 @@
+package ArchivosParaProbarCosas;
+
+public class ForQueAumentan2indices {
+
+	public static void main(String[] args) {
+		int j=10;
+		for (int i = 0; i < 10; i++,j++) {
+			System.out.println("I: "+i+" J: "+j);
+		}
+
+	}
+
+}
--- Clases en Uach/pruebasAñosAnteriores/examenes/Polinomio.java
+++ Clases en Uach/pruebasAñosAnteriores/examenes/Polinomio.java
@@ -9,17 +9,23 @@ public class Polinomio {
 		return sum;
 	}
 
-//	public static double evaluarPolinomioRango(double coeficientes[], double x1, double x2, double intervalo) {
-//		double sum = 0;
-//		for (double d : coeficientes) {
-//			sum += Math.pow(x1, d);
-//		}
-//		return sum;
-//	}
+	public static double[][] evaluarPolinomioRango(double coeficientes[], double x1, double x2, double intervalo) {
+		int n = (int)((x2-x1)/intervalo),j=0;
+		double sum[][] = new double[n][2];
+		for (double i = x1; i < x2; i+=intervalo, j++) {
+			sum[j][0]=i;
+			sum[j][1]=evaluarPolinomio(coeficientes, i);
+		}
+		return sum;
+	}
 	
 	public static void main(String[] args) {
-		double coeficientes[] = {2.0,-1,4.0,8};
+		double coeficientes[] = {2.0, -1, 4.1, 8};
 		System.out.println("La evaluacion del polinomio segun los coeficientes es: "+ evaluarPolinomio(coeficientes, 2));
-		double x1 = -1,x2 = 1;
+		double x1 = -2,x2 = 4;
+		double X[][] = evaluarPolinomioRango(coeficientes, x1, x2, 0.1);
+		for (int i = 0; i < X.length; i++) {
+			System.out.println("Punto ("+X[i][0]+" , "+X[i][1]+")");
+		}
 	}
 }
