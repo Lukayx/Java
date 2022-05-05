3c403d4af34c89455831bd59a15e5f63e30db0ae
Lukayx <olavarriafernando36@gmail.com>
2022-05-05 11:38:49 -0400

    blabla

--- AquiColocaréCosasUtiles/src/paraLaPrueba/Probar.java
+++ AquiColocaréCosasUtiles/src/paraLaPrueba/Probar.java
@@ -8,7 +8,8 @@ public class Probar {
 		Cuadrado obj3 = new Cuadrado(12,5);
 		Cuadrado obj4 = new Cuadrado(9,5);
 		Cuadrado obj5 = new Cuadrado(1,1);
-		Cuadrado array[] = {obj1,obj2,obj3,obj4,obj5};
+		Cuadrado obj6 = new Cuadrado(13,7);
+		Cuadrado array[] = {obj1,obj2,obj3,obj4,obj5,obj6};
 		Cuadrado.ordena(array);
 		for (Cuadrado i : array) {
 			System.out.println(i.area());
