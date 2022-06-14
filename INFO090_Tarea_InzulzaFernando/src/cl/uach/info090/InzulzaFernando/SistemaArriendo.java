292449c0910ba9bacb2717730ad9622fefa48ffc
Lukayx <olavarriafernando36@gmail.com>
2022-06-14 01:59:43 -0400

    tarea 5 actualizada

--- INFO090_Tarea_InzulzaFernando/src/cl/uach/info090/InzulzaFernando/SistemaArriendo.java
+++ INFO090_Tarea_InzulzaFernando/src/cl/uach/info090/InzulzaFernando/SistemaArriendo.java
@@ -1,20 +1,49 @@
-package cl.uach.info090.InzulzaFernando;
+	package cl.uach.info090.InzulzaFernando;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 
 import javax.swing.*;
+
 public class SistemaArriendo extends JFrame implements ActionListener{
 	
 	private static SistemaArriendo sistema = new SistemaArriendo();
 	
 	private SistemaArriendo() {
 		super("Ventana Genial");
-		setBounds(100, 100, 400, 300);
+		
+		//-----VENTANA-----
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setResizable(true);
-		setLayout(new BorderLayout());
-		JButton Boton = new JButton();
-		JPanel panel = new JPanel();
+		setTitle("Sistema Arriendo");
+		setSize(1000, 700);
+		setLocationRelativeTo(sistema);
+		setResizable(false);
+		
+		//-----BOTONES-----
+		JButton botonesArriendo[] = new JButton[16]; //ARREGLO DE BOTONES PARA ARRENDAR 
+		
+		//-----PANEL ARRIENDO-----
+		JPanel panel_Arriendo = new JPanel(); //Creacion de Panel
+		panel_Arriendo.setBackground(Color.GREEN);//Establecemos color al panel
+		panel_Arriendo.setLayout(new GridLayout(4,4,10,10)); 	
+		this.getContentPane().add(panel_Arriendo);//Agregamos el panel a la pantalla
+		
+		for (int i = 0; i < botonesArriendo.length; i++) {
+			botonesArriendo[i] = new JButton("Boton "+(i+1));
+			botonesArriendo[i].addActionListener(null);
+			panel_Arriendo.add(botonesArriendo[i]);
+		}
+		
+		JPanel panel_Info = new JPanel();
+		JPanel panel_Boletas = new JPanel();
+		
+		
+		
+		panel_Info.setLayout(new GridLayout(2,7,10,5));
+		//this.add(panel_Info);
+		//this.add(panel_Boletas);
+		
 		JTextField JTextField = new JTextField();
 		JLabel label = new JLabel();
 		JList lista = new JList();
